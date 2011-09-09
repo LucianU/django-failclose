3f045121305d522a2228e93bab656455ffc862aa
Lucian Ursu <lucian.ursu@gmail.com>
2011-09-09 17:30:06 +0300

    Added a utility function did some renaming

--- failclose/decorators.py
+++ failclose/decorators.py
@@ -1,3 +1,20 @@
-def safe(func):
-    func.safe = True
-    return func
+from failclose.permissions import RULES
+
+def safe(view):
+    view.safe = True
+    return view
+
+def is_safe(view):
+    # retrieving the view's app name and the corresponding views list
+    app_name = view.__module__.split('.')[-2]
+    views = RULES.get(app_name)
+
+    if views is not None:
+        # if the views list is empty, it means that all views
+        # are whitelisted
+        if not views or view.__name__ in views:
+            return True
+
+    if getattr(view, 'safe', None) is None:
+        return False
+
