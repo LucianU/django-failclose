244389f57c902c05e003ab1fdd88d40897baed3a
Lucian Ursu <lucian.ursu@gmail.com>
2011-09-13 12:00:02 +0300

    Added a setup.py file

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='django-failclose',
+    packages=['failclose'],
+    version='0.1',
+    description='Application that makes Django a failclose system',
+    author='Lucian Ursu',
+    author_email='lucian.ursu@gmail.com',
+    url='',
+    download_url='',
+    classifiers=[
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'Topic :: WWW/HTTP',
+        'Topic :: Utilities',
+    ],
+)
+
