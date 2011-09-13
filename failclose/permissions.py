dcb0be252f6180166db1747e5bc364c5fca9e663
Lucian Ursu <lucian.ursu@gmail.com>
2011-09-13 14:13:05 +0300

    Removed unnecessary files

--- failclose/permissions.py
+++
@@ -1,8 +0,0 @@
-# mapping between app names and the views considered safe
-# if the view doesn't belong to an app, use the project
-# name instead
-
-RULES = {
-    'bucket': ['index'],
-    'bark': ['index'],
-}
