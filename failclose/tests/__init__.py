26c75218b7254788848cc06c1d1513b812c586ab
Lucian Ursu <lucian.ursu@gmail.com>
2011-09-18 11:20:38 +0300

    Fixed typo

--- failclose/tests/__init__.py
+++ failclose/tests/__init__.py
@@ -1,3 +1,3 @@
-from faiclose.tests.base import (SafeDecoratorTest, GetProjectNameTest,
+from failclose.tests.base import (SafeDecoratorTest, GetProjectNameTest,
         GetAppNameTest, ValidateRulesTest, IsSafeTest, FailCloseMiddlewareTest,
         FailCloseAppTest)
